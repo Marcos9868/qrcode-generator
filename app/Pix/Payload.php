88d03d7eaac6dc56c2cd4bedd5716ace16b070d5
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-12-15 20:58:45 -0300

    Feat: private variables and setter's method

--- app/Pix/Payload.php
+++ app/Pix/Payload.php
@@ -3,6 +3,9 @@
 namespace App\Pix;
 
 class Payload {
+  /** 
+   * PIX Payload ID's
+  */
   const ID_PAYLOAD_FORMAT_INDICATOR = '00';
   const ID_MERCHANT_ACCOUNT_INFORMATION = '26';
   const ID_MERCHANT_ACCOUNT_INFORMATION_GUI = '00';
@@ -17,4 +20,94 @@ class Payload {
   const ID_ADDITIONAL_DATA_FIELD_TEMPLATE = '62';
   const ID_ADDITIONAL_DATA_FIELD_TEMPLATE_TXTID = '05';
   const ID_CRC16 = '05';
+
+  /**
+  * Pix Key
+  * @var string
+  */
+  private $pixKey;
+
+  /**
+  * Payment Description
+  * @var string
+  */
+  private $description;
+
+  /**
+  * Account person name
+  * @var string
+  */
+  private $merchantName;
+  
+  /**
+  * Account Person City
+  * @var string
+  */
+  private $merchantCity;
+  
+  /**
+  * Pix Id Transaction
+  * @var string
+  */
+  private $txtId;
+  
+  /**
+  * Transaction Value
+  * @var string
+  */
+  private $amount;
+
+  /**
+   * Method that define pixKey value
+   * @param string $pixKey 
+  */
+  public function setPixKey($pixKey) {
+    $this->pixKey = $pixKey;
+    return $this;
+  }
+
+  /**
+   * Method that set description value
+   * @param string $description 
+  */
+  public function setDescription($description) {
+    $this->description = $description;
+    return $this;
+  }
+  
+  /**
+   * Method that set merchantName value
+   * @param string $merchantName 
+  */
+  public function setMerchantName($merchantName) {
+    $this->merchantName = $merchantName;
+    return $this;
+  }
+  
+  /**
+   * Method that set merchantCity value
+   * @param string $merchantCity 
+  */
+  public function setMerchantCity($merchantCity) {
+    $this->merchantCity = $merchantCity;
+    return $this;
+  }
+  
+  /**
+   * Method that set txtId value
+   * @param string $txtId
+  */
+  public function setTxtId($txtId) {
+    $this->txtId = $txtId;
+    return $this;
+  }
+
+  /**
+   * Method that set amount value
+   * @param string $amount
+  */
+  public function setAmount($amount) {
+    $this->amount = $amount;
+    return $this;
+  }
 }
