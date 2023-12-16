50655d1d743714ffaffd888fea740e1d0b8ce74e
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-12-16 12:24:26 -0300

    Feat: HTML page that renderize QR Code

---
+++ page.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>QR Code Page</title>
+</head>
+<body style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+  <?php include 'index.php'; ?>
+</body>
+</html>
