31e721815308be5ce15acb7cb8ac383c6ca5e047
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-12-16 12:25:09 -0300

    Feat(Add): Mpdf lib and method that transform content to QR Code

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 
 require __DIR__.'/vendor/autoload.php';
 use \App\Pix\Payload;
+use Mpdf\QrCode\QrCode;
+use Mpdf\QrCode\Output;
 
+// main instance
 $objPayload = (new Payload)->setPixKey('12345678900')
                             ->setDescription('Payments')
                             ->setMerchantName('Marcos Melo Ferreira')
@@ -11,7 +14,21 @@ $objPayload = (new Payload)->setPixKey('12345678900')
                             ->setTxtId('CODE1234');
 
 
+// Payment Code
 $payloadQrCode = $objPayload->getPayload();
+
+// QR CODE
+$obQrCode = new QrCode($payloadQrCode);
+
+// QR Code Image
+$qrCodeImage = (new Output\Png)->output($obQrCode, 400);
+
+// Output as data URI
+$base64Image = base64_encode($qrCodeImage);
+
+// Output as data URI
+echo '<img src="data:image/png;base64,' . $base64Image . '" alt="QR Code">';
+
 echo "<pre>";
 print_r($objPayload);
 print_r($payloadQrCode);
